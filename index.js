d53f1711617560efd59e88e382893125d9fb7304
Albeiro Lozano <Albeiro036@gmail.com>
2024-03-24 06:35:42 -0500

    fc2

---
+++ index.js
@@ -0,0 +1,15 @@
+import { faker } from "@faker-js/faker";
+import colors from "colors";
+
+colors.enabled = true;
+
+export function send_random_number() {
+    const data = {
+        number: faker.string.uuid(),
+        name: faker.internet.userName(),
+    }
+    console.log(`${data.number} - ${data.name}`.green.bold);
+    return data;
+}
+
+send_random_number()
